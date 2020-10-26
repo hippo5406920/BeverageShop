5373fdf7c9fe3a6daceba9166649590e1ba77227
UserFirebaseToLogin <hippo5406920@icloud.com>
2020-10-26 16:11:15 +0800

    BeverageShop 1.0

---
+++ BeverageShop/OrderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderTableViewCell.swift
+//  BeverageShop
+//
+//  Created by CHEN on 2020/10/26.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
